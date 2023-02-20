81da1b3536683ac3ecf9054990b540a973655c90
Xavi Puig <xpuiig14@gmail.com>
2023-02-20 05:00:02 +0100

    Add an Autoplay for the slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,11 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Product } from 'src/app/models/Product';
+
 
 @Component({
   selector: 'app-slider',
   // templateUrl: `./slider.component.html`,
   template: `
-    <ion-slides [pager]="true" class="slider">
+    <ion-slides [pager]="true" class="slider" #slider (ionSlideDidChange)="onSlideDidChange()">
       <ion-slide *ngFor="let item of products" class="slide" style.background-image="url('{{item.image}}')">
           <div class = "box">
             <h1>{{item.name}}</h1>
@@ -16,12 +17,40 @@ import { Product } from 'src/app/models/Product';
   `,
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
+
+  
 
   @Input() products: Product[] = [];
+  @ViewChild('slider') slider: any;
+  
 
   
   constructor() { 
+  
   }
+  ngOnInit() {
+    // Establece el intervalo en el que se cambiarán las imágenes
+    setInterval(() => {
+      // Mueve el slider al siguiente slide
+      this.slider.slideNext();
+    }, 5000); // Establece el intervalo de tiempo en milisegundos (en este caso, 3 segundos)
+  }
+
+  onSlideDidChange() {
+    // Verifica si el slide actual es el último
+    this.slider.getActiveIndex().then((index: number) => {
+      if (index === this.products.length - 1) {
+        // Mueve el slider al primer slide
+        setTimeout(() => {
+          this.slider.slideTo(0);
+        }, 5000);
+      }
+    });
+  }
+  
+
+  
+
 
 }
