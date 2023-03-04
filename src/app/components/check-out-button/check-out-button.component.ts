b26f8ef54d7bb961a2870961d75da0088dd246cf
Xavi Puig <xpuiig14@gmail.com>
2023-03-04 23:12:46 +0100

    Add check-out-button as a component

--- src/app/components/check-out-button/check-out-button.component.ts
+++ src/app/components/check-out-button/check-out-button.component.ts
@@ -2,15 +2,8 @@ import { Component, EventEmitter, HostBinding, Input, OnInit, Output } from '@an
 
 @Component({
     selector: 'app-check-out-button',
-    template: `
-        <ion-fab vertical="bottom" horizontal="end" slot="fixed">
-            <ion-fab-button (click)="onClick()">
-                <ion-icon name="cart"></ion-icon>
-                <ion-badge>{{ badgeValue }}</ion-badge>
-            </ion-fab-button>
-        </ion-fab>
-        `,
-    // styleUrls: ['./check-out-button.component.scss'],
+    templateUrl: './check-out-button.component.html',
+    styleUrls: ['./check-out-button.component.scss'],
 })
 
 export class CheckOutButtonComponent {
@@ -18,9 +11,6 @@ export class CheckOutButtonComponent {
     @Input() badgeValue: number = 0;
     @Output() onClickEvent: EventEmitter<any> = new EventEmitter();
 
-    @HostBinding('attr.vertical') vertical: string = 'bottom';
-    @HostBinding('attr.horizontal') horizontal: string = 'end';
-    @HostBinding('attr.slot') slot: string = 'fixed';
     constructor() { }
 
     onClick = () => this.onClickEvent.emit();
